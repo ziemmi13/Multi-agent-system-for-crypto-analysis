793d9d2a39436e9d03c7d80d120d8fd17e4f4389
ziemmi13 <ziemmi13@gmail.com>
2026-01-17 17:57:58 +0100

    Added ability to use openai model in root_agent

--- root_agent/agent.py
+++ root_agent/agent.py
@@ -1,5 +1,7 @@
 from google.adk.agents import LlmAgent
 from google.adk.tools.agent_tool import AgentTool
+# for external LLM providers
+from google.adk.models.lite_llm import LiteLlm
 
 from . import prompt
 from .sub_agents.business_analyst_1.agent import business_analyst_1
@@ -9,9 +11,21 @@ from .sub_agents.policy_enforcer.agent import policy_enforcer
 from .sub_agents.trader.agent import trader
 from .sub_agents.trader.tools.trade import log_policy_rejection
 from .tools.trade_formatter import format_trade_request
+from dotenv import load_dotenv
+import pathlib
+import os
+
+# Load environment variables 
+root_dir = pathlib.Path(__file__).parent
+load_dotenv(root_dir / '.env')
+
+# Models
+gemini_model = 'gemini-2.5-flash'
+open_ai_model = model=LiteLlm('openai/gpt-4.1',
+                  api_key=os.getenv("OPENAI_API_KEY"))
 
 root_agent = LlmAgent(
-    model='gemini-2.5-flash',
+    model=open_ai_model,
     name='root_agent',
     description='Oversees business and technical cryptocurrencies analysts.',
     instruction=prompt.ROOT_AGENT_PROMPT,
